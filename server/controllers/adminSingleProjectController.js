efe8f0f765000c2ddf42db34285c11c0bc8b4002
Meke Brown <mekesiabrown@gmail.com>
2022-03-10 20:35:04 -0500

    Add renamed controller files

---
+++ server/controllers/adminSingleProjectController.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql');
+const config = require('../database/db_details');
+
+const {details, queryDatabase} = config;
+
+// db connection; Add a server limiter with createPool
+const conn = new mysql.createConnection(details);
+
+conn.connect((err) => {
+  if (err) {
+    console.log("Cannot connect. Error: " + err);
+  } else {
+    console.log("Connection established for admin.");
+  }
+});
+
+exports.adminSingleProjectController = (req, res) => {
+  res.json({
+    adminSingleProject: ["admin single project 1", "admin single project 2", "this", "works?"]
+});
+}
