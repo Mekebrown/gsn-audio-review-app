bbe4589981d8cdc402cee816a47a1599a225f69e
Meke Brown <mekesiabrown@gmail.com>
2022-03-22 12:58:01 -0400

    Initial commit of testing change

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,23 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders "Open App" button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const buttonElement = screen.getByText(/Open App/i);
+  
+  expect(buttonElement).toBeInTheDocument();
+  expect(screen.getByRole('button')).toHaveTextContent(/Open App/i);
+  expect(screen.getByRole('button')).not.toBeDisabled();
 });
+
+// test('has expected number of client-side routes', () => {
+
+// });
+
+// test('has redirect links working as expected', () => {
+// // Every button click's link
+// // Every a tag link?
+// // Shows correct user data dependent upon the url params
+// });
