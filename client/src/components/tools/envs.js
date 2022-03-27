2ca473e7918eac04cde742f42dd30ca8d5cea6d2
Meke Brown <mekesiabrown@gmail.com>
2022-03-27 01:37:38 -0400

    Separate out data retrieval/referencing

--- client/src/components/tools/envs.js
+++ client/src/components/tools/envs.js
@@ -1,13 +1,20 @@
-const testAudio = "https://ia902203.us.archive.org/11/items/testmp3testfile/mpthreetest";
+const testAudio = "https://ia902203.us.archive.org/11/items/testmp3testfile/mpthreetest.";
 
 const {
     REACT_APP_FULL_TITLE, 
     REACT_APP_SERVER_URL
 } = process.env;
 
-const projectReviewingPath = "/usingle/";
-const indvlUserPath = "/user_path";
+const adminPath = "/admin";
+const adminIndvlViewinglePath = "/asingle";
+const allNotesPath = "/notes";
+const allProjectsPath = "/projects";
+const allUsersPath = "/users";
+const indexPath = "/";
 const indvlProjectPath = "project_path";
+const indvlUserPath = "/user_path";
+const projectReviewingPath = "usingle/";
+const uploadMediaPath = "/uploadmedia";
 
 export default testAudio; 
 
@@ -17,7 +24,14 @@ export {
 };
 
 export {
+    adminPath,
+    adminIndvlViewinglePath,
+    allNotesPath,
+    allProjectsPath,
+    allUsersPath,
+    indexPath,
     indvlProjectPath,
     indvlUserPath,
-    projectReviewingPath
+    projectReviewingPath,
+    uploadMediaPath
 };
