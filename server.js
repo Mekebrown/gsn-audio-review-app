79c5d2b65165f3be5f7a91d32cc45af3abf63721
Meke Brown <mekesiabrown@gmail.com>
2022-06-16 10:48:03 -0400

    add dummy data for prod testing

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mysql = require("mysql");
 const cors = require('cors');
 const path = require('path');
 const bp = require('body-parser');
+const someInfo = require("./server/database/dummy_info");
 
 require('dotenv').config();
 
@@ -92,32 +93,36 @@ app.post("/", (req, res) => {
 app.get("/usingle/:media_id", (req, res) => {
   const mediaId = parseInt(req.params.media_id);
 
-  //media - Only one result; We need the following: media_id, file_name,  file_directory, user_id, project_name, media_desc, and all_note_ids
-  let media_query_statement = "SELECT * FROM media WHERE media_id = ?";
+  if (process.env.NODE_ENV === 'production') {
+    res.send(someInfo);
+  } else {
+    //media - Only one result; We need the following: media_id, file_name,  file_directory, user_id, project_name, media_desc, and all_note_ids
+    let media_query_statement = "SELECT * FROM media WHERE media_id = ?";
 
-  // ratings - Zero or one result; We need the following: thumb_rating_id, media_id, user_id, thumb_rating
-  let ratings_query_statement = "SELECT * FROM ratings WHERE media_id = ?";
+    // ratings - Zero or one result; We need the following: thumb_rating_id, media_id, user_id, thumb_rating
+    let ratings_query_statement = "SELECT * FROM ratings WHERE media_id = ?";
 
-  // notes - Zero or more results; We need the following: media_id, user_id, note_id, note_last_updated, note_last_retrieved, note_body, note_timestamp
-  let notes_query_statement = `SELECT 
-                                note_id, note_body, note_last_updated, note_timestamp, note_last_retrieved, note_is_deleted
-                                FROM notes 
-                                WHERE media_id = ? 
-                                AND note_is_deleted = 'no' 
-                                ORDER BY note_last_retrieved 
-                                DESC 
-                                LIMIT 5
-  `;
-  
-  getQueryValues(media_query_statement, [mediaId])
-  .then(() => getQueryValues(ratings_query_statement, [mediaId]))
-  .then(() => getNotesQueryValues(notes_query_statement, [mediaId]))
-  .then((rows) => {
-    dataToSend = {...dataToSend, totalNotesFromServer: rows};
+    // notes - Zero or more results; We need the following: media_id, user_id, note_id, note_last_updated, note_last_retrieved, note_body, note_timestamp
+    let notes_query_statement = `SELECT 
+                                  note_id, note_body, note_last_updated, note_timestamp, note_last_retrieved, note_is_deleted
+                                  FROM notes 
+                                  WHERE media_id = ? 
+                                  AND note_is_deleted = 'no' 
+                                  ORDER BY note_last_retrieved 
+                                  DESC 
+                                  LIMIT 5
+    `;
+    
+    getQueryValues(media_query_statement, [mediaId])
+    .then(() => getQueryValues(ratings_query_statement, [mediaId]))
+    .then(() => getNotesQueryValues(notes_query_statement, [mediaId]))
+    .then((rows) => {
+      dataToSend = {...dataToSend, totalNotesFromServer: rows};
 
-    res.send(dataToSend);
-  })
-  .catch((err) => console.log("Promise rejection error: " + err));
+      res.send(dataToSend);
+    })
+    .catch((err) => console.log("Promise rejection error: " + err));
+  }
 });
 
 if (process.env.NODE_ENV === 'production') {
