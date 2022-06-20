08c7b1f2c1d8cebee7fd77a934d4711c4e616913
Meke Brown <mekesiabrown@gmail.com>
2022-06-20 14:08:06 -0400

    remove code not focused on yet

--- client/src/App.js
+++ client/src/App.js
@@ -3,42 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 
 // Variables
-import { 
-  adminPath,
-  adminIndvlViewinglePath,
-  allNotesPath,
-  allProjectsPath,
-  allUsersPath,
-  indexPath,
-  projectReviewingPath, 
-  REACT_APP_FULL_TITLE,
-  uploadMediaPath 
-} from "./components/tools/envs";
-
-// Admin
-import AdminSingleProject from "./components/Admin/AdminSingleProject"; 
-import Admin from "./components/Admin/Admin";
-import UploadMedia from "./components/Admin/UploadMedia";
-import Notes from "./components/Admin/Notes";
-import Projects from "./components/Admin/AllProjects";
+import { indexPath, REACT_APP_FULL_TITLE } from "./components/tools/envs";
 
 // General
 import Index from "./components/Index"; 
 
 // User
 import UserSingleProject from "./components/User/UserSingleProject";
-import Users from "./components/User/Users";
 
 function App() {
-
-  const LocationForHome = () => {
-    if (process.env.NODE_ENV === 'production') { 
-      return <UserSingleProject mediaId={1} />
-    } else {
-      return <Index />;
-    }
-  };
-
   return (
     <div className="app">
       <header>
@@ -46,15 +19,9 @@ function App() {
       </header>
       <Router>
         <Routes>
-          <Route path={adminPath} element={<Admin />} />
-          <Route path={adminIndvlViewinglePath} element={<AdminSingleProject />} />
-          <Route path={uploadMediaPath} element={<UploadMedia />} />
-          <Route path={allProjectsPath} element={<Projects />} />
-          <Route index path={indexPath} element={<LocationForHome />} />
-          <Route path={allNotesPath} element={<Notes />} />
-          <Route path={projectReviewingPath + 1} element={<UserSingleProject mediaId={1} />} />
-          <Route path={allUsersPath} element={<Users />} />
-          
+          <Route index path={indexPath} element={<Index />} />
+          <Route path="http://localhost:3001/usingle/:media_id" element={<UserSingleProject mediaId={1} />} />
+
           {/* For non-existent routes */}
           <Route 
               path="*" 
