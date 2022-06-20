7355634c61282dfb2e6e0643526e3bcf587e113e
Meke Brown <mekesiabrown@gmail.com>
2022-06-20 14:06:50 -0400

    move code back

--- server.js
+++ server.js
@@ -85,8 +85,39 @@ app.post("/", (req, res) => {
   res.send("Cool");
 });
 
+app.get("/usingle/:media_id", (req, res) => {
+  const mediaId = parseInt(req.params.media_id);
+
+  //media - Only one result; We need the following: media_id, file_name,  file_directory, user_id, project_name, media_desc, and all_note_ids
+    let media_query_statement = "SELECT * FROM media WHERE media_id = ?";
+
+    // ratings - Zero or one result; We need the following: thumb_rating_id, media_id, user_id, thumb_rating
+    let ratings_query_statement = "SELECT * FROM ratings WHERE media_id = ?";
+
+    // notes - Zero or more results; We need the following: media_id, user_id, note_id, note_last_updated, note_last_retrieved, note_body, note_timestamp
+    let notes_query_statement = `SELECT 
+                                  note_id, note_body, note_last_updated, note_timestamp, note_last_retrieved, note_is_deleted
+                                  FROM notes 
+                                  WHERE media_id = ? 
+                                  AND note_is_deleted = 'no' 
+                                  ORDER BY note_last_retrieved 
+                                  DESC 
+                                  LIMIT 5
+    `;
+    
+    getQueryValues(media_query_statement, [mediaId])
+    .then(() => getQueryValues(ratings_query_statement, [mediaId]))
+    .then(() => getNotesQueryValues(notes_query_statement, [mediaId]))
+    .then((rows) => {
+      dataToSend = {...dataToSend, totalNotesFromServer: rows};
+
+      res.send(dataToSend);
+    })
+    .catch((err) => console.log("Promise rejection error: " + err));
+});
+
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('/client/build'));
+  app.use(express.static("/client/build"));
 
   app.get("/", (req, res) => {
       res.send({
@@ -121,37 +152,6 @@ if (process.env.NODE_ENV === 'production') {
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-} else {
-  app.get("/usingle/:media_id", (req, res) => {
-    const mediaId = parseInt(req.params.media_id);
-
-    //media - Only one result; We need the following: media_id, file_name,  file_directory, user_id, project_name, media_desc, and all_note_ids
-      let media_query_statement = "SELECT * FROM media WHERE media_id = ?";
-
-      // ratings - Zero or one result; We need the following: thumb_rating_id, media_id, user_id, thumb_rating
-      let ratings_query_statement = "SELECT * FROM ratings WHERE media_id = ?";
-
-      // notes - Zero or more results; We need the following: media_id, user_id, note_id, note_last_updated, note_last_retrieved, note_body, note_timestamp
-      let notes_query_statement = `SELECT 
-                                    note_id, note_body, note_last_updated, note_timestamp, note_last_retrieved, note_is_deleted
-                                    FROM notes 
-                                    WHERE media_id = ? 
-                                    AND note_is_deleted = 'no' 
-                                    ORDER BY note_last_retrieved 
-                                    DESC 
-                                    LIMIT 5
-      `;
-      
-      getQueryValues(media_query_statement, [mediaId])
-      .then(() => getQueryValues(ratings_query_statement, [mediaId]))
-      .then(() => getNotesQueryValues(notes_query_statement, [mediaId]))
-      .then((rows) => {
-        dataToSend = {...dataToSend, totalNotesFromServer: rows};
-
-        res.send(dataToSend);
-      })
-      .catch((err) => console.log("Promise rejection error: " + err));
   });
 }
 
