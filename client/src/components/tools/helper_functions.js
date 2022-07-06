45b58bd9cfc49cc54195d663f68c580f0c1f7bde
Meke Brown <mekesiabrown@gmail.com>
2022-07-05 23:25:26 -0400

    refactor: move code to dedicated helper file

---
+++ client/src/components/tools/helper_functions.js
@@ -0,0 +1,13 @@
+const values = {
+    projectName: "Track Audio",
+    createdOn: null,
+    currentTime: 0
+};
+
+const sources = [
+    {item: 1, ext: "mp3", type: "audio/mpeg"}, 
+    {item: 2, ext: "ogg", type: "audio/ogg"}
+];
+
+export default values;
+export { sources };
