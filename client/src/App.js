2fe7967575ffc2c8596cb9c0526db9c17f7a52b1
Meke Brown <mekesiabrown@gmail.com>
2022-07-08 16:18:57 -0400

    refactor: refactor the key entry code

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -9,7 +9,22 @@ import './App.css';
 import { indexPath, REACT_APP_FULL_TITLE } from "./components/tools/envs";
 import UserSingleProject from "./components/User/UserSingleProject";
 
-function App() {
+const App = () => {
+  const [key, setKey] = useState(null);
+  const [adminEntry, setAdminEntry] = useState(false);
+  let adminKey = useRef(null);
+
+  const handleKeyEntry = () => { setKey(adminKey.current.value); };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(key);
+
+    if (key === "gifted") {
+      setAdminEntry(true);
+    }
+  };
+
   return (
     <Router>
       <div>
@@ -21,7 +36,23 @@ function App() {
         <hr />
 
         <Routes>
-          <Route exact path={indexPath} element={<h2>{REACT_APP_FULL_TITLE}</h2>} />
+          <Route exact path={indexPath} element={
+            <>
+              <h2>{REACT_APP_FULL_TITLE}</h2>
+              
+              {!adminEntry ? (<form onSubmit={handleFormSubmit} method="POST">
+                <label htmlFor="key">
+                  <input type="text" name="key" id="key" placeholder="Login key" ref={adminKey} onChange={handleKeyEntry} />
+                </label>
+
+                <label htmlFor="submit">
+                  <input type="submit" name="submit" id="submit" value="Enter" />
+                </label>
+              </form>) : (
+                <p>Welcome Lance</p>
+              )}
+            </>
+          } />
           <Route path="/usingle/:mediaId" element={<UserSingleProject mediaId={1} />} />
         </Routes>
       </div>
