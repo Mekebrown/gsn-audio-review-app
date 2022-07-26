83f9559b3685ca5645fa0f17b56da0569c4605d8
Meke Brown <mekesiabrown@gmail.com>
2022-07-26 01:11:34 -0400

    refactor: comment out and test different syntax

--- server.js
+++ server.js
@@ -63,12 +63,19 @@ const showTimes = () => {
 };
 
 if (isProduction) {
-  app.use(express.static(path.join(__dirname, "client/build")));
-  
-  app.get("/times", (req, res) => res.send("Interesting production"));
+  // app.use(express.static(path.join(__dirname, "client/build")));
+  // app.use(express.static('client/build'));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  
+  // app.get("/times", (req, res) => res.send("Interesting production"));
+
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  // });
+  app.use(express.static("client/build"));
+  
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
   });
 } else {  
   let dataToSend = {}; // Let to allow overwriting later
