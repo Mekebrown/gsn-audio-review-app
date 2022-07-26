319c95c1b916df054b734109188041b7ca398bf0
Meke Brown <mekesiabrown@gmail.com>
2022-07-25 20:31:58 -0400

    refactor: move CORS sifting code to own file

--- server/tools/cors_options.js
+++ server/tools/cors_options.js
@@ -4,6 +4,21 @@ const allowList = [
     'https://intense-forest-28148.herokuapp.com/'
 ];
 
+
+const corsOptions = {
+    origin: (origin, callback) => {
+      console.log("** Origin of request " + origin);
+  
+      if (allowList.indexOf(origin) !== -1 || !origin) {
+        console.log("Origin acceptable");
+        callback(null, true);
+      } else {
+        console.log(allowList.indexOf(origin));
+        callback(new Error("Not allowed by CORS"));
+      }
+    }
+};
+
 module.exports = {
-    allowList
+    corsOptions
 }
