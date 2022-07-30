b4d29f8500d52793fae9b0d64440982fa6bd08aa
Meke Brown <mekesiabrown@gmail.com>
2022-07-30 17:55:30 -0400

    refactor: add logging

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require('cors');
 const path = require('path');
 const bp = require('body-parser');
-const { Sequelize, DataTypes, Model } = require("sequelize");
+const { Sequelize, DataTypes, Op } = require("sequelize");
 const fileupload = require("express-fileupload");
 const {
   media_upload_query_statement,
@@ -17,6 +17,7 @@ const {
   media_query_statement_insert
 } = require("./server/database/query_strings.js");
 const { Pool } = require('pg');
+const fs = require("fs");
 require('dotenv').config();
 
 app.use(express.json());
@@ -25,7 +26,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'client', 'public')));
-app.use(express.static('files'));
+app.use(express.static('./files'));
+app.use(express.static('./files/logs'));
 app.use(cors(require("./server/tools/cors_options")));
 
 const pool = new Pool({
@@ -48,7 +50,19 @@ const sequelize = new Sequelize(
 
 sequelize.authenticate()
 .then(() => console.log('Connection has been established successfully.'))
-.catch((error) => console.error('Unable to connect to the database:', error));
+.catch((err) => {  
+  logger({ 
+    location: "sequelize_auth",
+    req: "sequelize variable: " + sequelize + 
+          "| process.env.DB_DATABASE: " + process.env.DB_DATABASE + 
+          "| process.env.DB_USERNAME: " + process.env.DB_USERNAME + 
+          "| process.env.DB_PASSWORD: " + process.env.DB_PASSWORD +
+          "| process.env.DB_HOST: " + process.env.DB_HOST,
+    message: err 
+  });
+  
+  console.error('Unable to connect to the database:', err); 
+});
 
 const isProduction = process.env.NODE_ENV === "production";
 
@@ -76,8 +90,7 @@ if (isProduction) {
     media_id:         { type: DataTypes.INTEGER, allowNull: false },
     note_body:        { type: DataTypes.STRING(5000), allowNull: false },
     timestamp:        { type: DataTypes.STRING, allowNull: false },
-    last_retrieved:   { type: DataTypes.DATE, allowNull: true },
-    last_updated:     { type: DataTypes.DATE, allowNull: true },
+    last_retrieved:   { type: DataTypes.DATE, allowNull: true }
   });
 
   const Media = sequelize.define('Media', {
@@ -96,6 +109,13 @@ if (isProduction) {
     return new Promise((resolve, reject) => {
       conn.query(queryStatement, params, (err, rows) => {                                                
           if (err || rows === undefined) {
+            logger({
+              location: "getQueryValues", 
+              req: "Query statement: " + queryStatement + "| Params: " + params, 
+              res: "Rows: " + rows,
+              message: err
+            });
+
             reject(new Error(err));
           } else {
             dataToSend = {...dataToSend, ...rows[0]};
@@ -110,6 +130,12 @@ if (isProduction) {
     return new Promise((resolve, reject) => {
       conn.query(queryStatement, params, (err, rows) => {                                                
           if (err || rows === undefined) {
+            logger({
+              location: "getNotesQueryValues", 
+              req: "Query statement: " + queryStatement + "| Params: " + params, 
+              res: "Rows: " + rows,
+              message: err
+            });
             reject(new Error(err));
           } else {
             resolve(rows);
@@ -121,6 +147,57 @@ if (isProduction) {
 
   app.get("/usingle", (req, res) => {
     const mediaId = parseInt(req.query.media_id) ? parseInt(req.query.media_id) : 1;
+
+    const userId = 1;
+
+    Media.findOne({
+      where: {
+        id: mediaId
+      }
+    })
+    .then((res) => dataToSend = {...dataToSend, ...res.dataValues})
+    .catch((err) => {      
+      logger({
+        location: "get_usingle_mediaQuery", 
+        req: req.query, 
+        res: "Promise rejection error (Media)",
+        headers: req.rawHeaders[9] + "|" + 
+                  req.rawHeaders[13] + "|" + 
+                  req.rawHeaders[21] + "|" + 
+                  req.rawHeaders[22] + "-" + 
+                  req.rawHeaders[23],
+        message: err
+      });
+      
+      console.error("Promise rejection error (Media): " + err); });
+
+    Note.findAll({
+      order: [
+        ["updatedAt", "DESC"]
+      ],
+      where: {
+        [Op.and]: [
+          {media_id: mediaId},
+          {user_id: userId},
+        ]
+      }
+    })
+    .then((res) => dataToSend = {...dataToSend, ...res.dataValues})
+    .then(() => res.send(dataToSend))
+    .catch((err) => {
+      logger({
+        location: "get_usingle_noteQuery", 
+        req: req.query, 
+        res: "Promise rejection error (Note)",
+        headers: req.rawHeaders[9] + "|" + 
+                  req.rawHeaders[13] + "|" + 
+                  req.rawHeaders[21] + "|" + 
+                  req.rawHeaders[22] + "-" + 
+                  req.rawHeaders[23],
+        message: err
+      });
+      
+      console.error("Promise rejection error (Note): " + err); });
   });
 
   app.post("/usingle", (req, res) => {
@@ -162,10 +239,38 @@ if (isProduction) {
       .then(() => getNotesQueryValues(`INSERT INTO notes (note_body, note_last_updated, note_timestamp, note_last_retrieved, user_id, media_id, note_created_on) VALUES (?, ?, ?, ?, ?, ?, ?)`, [ note_body, converted_datetime, note_timestamp, converted_datetime, user_id, media_id, converted_datetime]))
       .then(() => getNotesQueryValues("SELECT note_id, note_last_updated FROM notes WHERE note_last_updated = ? LIMIT 1", [converted_datetime]))
       .then((note_info_retrieved_from_query) => note_id_to_work_with = note_info_retrieved_from_query[0].note_id)
-      .catch(() => res.status(500).send({ message: "Note not saved", code: 200 }));
+      .catch((err) => {
+        logger({
+          location: "post_usingle", 
+          req: req.query, 
+          res: "Note not saved",
+          headers: req.rawHeaders[9] + "|" + 
+                    req.rawHeaders[13] + "|" + 
+                    req.rawHeaders[21] + "|" + 
+                    req.rawHeaders[22] + "-" + 
+                    req.rawHeaders[23],
+          message: err
+        }); 
+        
+        res.status(500).send({ message: "Note not saved", code: 200 }); 
+      });
     } else {
       getNotesQueryValues(`UPDATE notes SET note_body = ?, note_last_updated = ?, note_timestamp = ?, note_last_retrieved = ? WHERE note_id = ?`, [note_body, converted_datetime, note_timestamp, converted_datetime, note_id_to_work_with])
-      .catch((err) => res.status(500).send({ message: "Note not saved", code: 200 }));
+      .catch((err) => {
+        logger({
+          location: "post_usingle_getNotesQueryValues", 
+          req: req.query, 
+          res: "Note not saved",
+          headers: req.rawHeaders[9] + "|" + 
+                    req.rawHeaders[13] + "|" + 
+                    req.rawHeaders[21] + "|" + 
+                    req.rawHeaders[22] + "-" + 
+                    req.rawHeaders[23],
+          message: err
+        });
+        
+        res.status(500).send({ message: "Note not saved", code: 200 }); 
+      });
     }
   });
 
@@ -194,10 +299,40 @@ if (isProduction) {
       .then(() => {
         res.status(200).send({ message: "File Uploaded", code: 200 });
       })
-      .catch((err) => console.log("Promise rejection error: " + err));
+      .catch((err) => {
+        logger({
+          location: "post_media", 
+          req: req.query, 
+          res: "Promise rejection error",
+          headers: req.rawHeaders[9] + "|" + 
+                    req.rawHeaders[13] + "|" + 
+                    req.rawHeaders[21] + "|" + 
+                    req.rawHeaders[22] + "-" + 
+                    req.rawHeaders[23],
+          message: err
+        });
+        
+        console.error("Promise rejection error: " + err) 
+      });
     });
   });
 }
+
+const logger = (details) => {
+  let current = ((new Date()).toLocaleString()).replace(/\D*/g, "");
+  let file_name = `${details.location}_${current}.log`;
+
+  let log_data = {
+    message: details.message,
+    req: details.req ? details.req : "N/A",
+    res: details.res ? details.res : "N/A",
+    headers: details.headers ? details.headers : "N/A",
+  };
+
+  fs.writeFile(file_name, JSON.stringify(log_data, null, '\t') + "\n===============\n", "utf8", (error, data) => {
+    console.log("Write complete"); console.log(error); console.log(data); 
+  });
+};
 
 app.listen(process.env.PORT || 3001, function() {
   console.log(process.env.SERVER_PORT);
