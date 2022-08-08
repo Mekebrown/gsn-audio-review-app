a109529d943d62fdd465aeb4debf1817ead06c0c
Meke Brown <mekesiabrown@gmail.com>
2022-08-07 23:02:31 -0400

    refactor: rename file

---
+++ client/src/components/tools/vars.js
@@ -0,0 +1,32 @@
+const testAudio_testing = "../tools/wearenotokay.mp3";
+
+const { REACT_APP_FULL_TITLE } = process.env;
+
+const adminPath = "/review";
+const singleMediaPath = "/usingle";
+const adminIndvlViewinglePath = "/asingle";
+const allNotesPath = "/notes";
+const allProjectsPath = "/projects";
+const allUsersPath = "/users";
+const indexPath = "/";
+const indvlProjectPath = "project_path";
+const indvlUserPath = "/user_path";
+const uploadMediaPath = "/review/add-media";
+const mediaId = 1;
+
+export default REACT_APP_FULL_TITLE;
+
+export {
+    adminPath,
+    adminIndvlViewinglePath,
+    allNotesPath,
+    allProjectsPath,
+    allUsersPath,
+    indexPath,
+    indvlProjectPath,
+    indvlUserPath,
+    singleMediaPath,
+    uploadMediaPath,
+    mediaId,
+    testAudio_testing
+};
