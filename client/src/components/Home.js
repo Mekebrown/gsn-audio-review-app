05ef0992648dc7e580683dbec811c1d60b71461c
Meke Brown <mekesiabrown@gmail.com>
2022-08-23 18:16:43 -0400

    refactor: update styling and add font awesome icons

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,11 @@
 import { useContext, useState } from "react";
 import axios from "axios";
 import { UserContext } from "./tools/helper_functions";
+import UserSingleProject from "./User/UserSingleProject";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+
 import styles from "./Home.css";
 import classNames from 'classnames/bind';
 
@@ -14,17 +19,11 @@ const Home = () => {
 
     let cx = classNames.bind(styles);
 
-    let unField = cx({ 
-      displayInline: !isMobile
-    });
+    let unField = cx({ displayInline: !isMobile });
 
-    let pwField = cx({ 
-        displayInline: !isMobile 
-    });
+    let pwField = cx({ displayInline: !isMobile });
 
-    let loginFieldDivs = cx({ 
-        displayFlex: !isMobile 
-    });
+    let loginFieldDivs = cx({ displayFlex: !isMobile });
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -78,39 +77,49 @@ const Home = () => {
             <main className="cont">
                 <section className="sect">
                     <p>
-                        <span>Gifted Sounds</span> gives you instant access to the audio, video, images, ANY media you create, from the moment it’s ready for your eyes and ears. 
+                        <span className="yellow">Gifted Sounds</span> gives you instant access to the audio, video, images, ANY media you create, from the moment it’s ready for your eyes and ears. 
                     </p>
-                    <small>
+                    <small class="specialColoring">
                         Enter your given password to review your project...
                     </small>
 
                     <form onSubmit={handleSubmit}>
                         {userMsg}
-                        <br />
                         <div className={loginFieldDivs}>
                             <label className={unField}>
-                                Your First Name: 
-                                <br />
-                                <input type="text" placeholder="Lance, gifted, etc." onChange={(e) => setInputUN(e.target.value)} minLength="8" maxLength="50" autoFocus required />
+                                
+                                <input type="text" placeholder="&#xF007; &nbsp; Lance, gifted, etc." onChange={(e) => setInputUN(e.target.value)} minLength="3" maxLength="50" autoFocus required style={{fontFamily: "Arial, 'Font Awesome 5 Free'", padding: "10px"}} />
                             </label>
 
+                            {' '} 
+
                             <label className={pwField}>
-                                Given Password: 
-                                <br />
-                                <input type="text" placeholder="Password" onChange={(e) => setInputPW(e.target.value)} minLength="8" maxLength="50" required />
+                                <input type="text" placeholder="&#xf044; &nbsp; Password" onChange={(e) => setInputPW(e.target.value)} minLength="8" maxLength="50" required style={{fontFamily: "Arial, 'Font Awesome 5 Free'", padding: "10px"}} />
                             </label>
                         </div>
                         <br /> 
-                        <button type="submit">Login</button>
+                        <button type="submit">
+                            <FontAwesomeIcon icon={faPaperPlane} />
+                             {' '} 
+                            Login
+                        </button>
                     </form>
                 </section>
 
                 <section className="sect animation">
-                    <div>Animation goes here</div>
+                    <div className="largePurple"></div>
+                    <div className="largWhite"></div>
+                    <div className="purpleCircle"></div>                   
+                    <div className="purpleC2"></div>
+                    <div className="yellowCircle"></div>                   
+                    <div className="smallWhite"></div>
+                    <div className="tinyWhite"></div>
+                    
+                    <svg className="playCircle" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle-play" class="svg-inline--fa fa-circle-play playCircle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#c98f27" d="M512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM176 168V344C176 352.7 180.7 360.7 188.3 364.9C195.8 369.2 205.1 369 212.5 364.5L356.5 276.5C363.6 272.1 368 264.4 368 256C368 247.6 363.6 239.9 356.5 235.5L212.5 147.5C205.1 142.1 195.8 142.8 188.3 147.1C180.7 151.3 176 159.3 176 168V168z"></path></svg>
                 </section>
             </main>
             : 
-            <button onClick={() => setUserId(null)}>Log Out</button>}
+            <UserSingleProject mediaId={1} />}
         </>
     );
 };
