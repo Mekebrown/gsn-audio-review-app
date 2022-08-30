bc79bbe707c4f2018ab5d2c9083e2cfe67a01257
Meke Brown <mekesiabrown@gmail.com>
2022-08-30 01:48:11 -0400

    refactor: gut page for future

--- client/src/components/Admin/Admin.js
+++ client/src/components/Admin/Admin.js
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import Home from "../Home";
 import { UserContext } from "../tools/helper_functions";
-import singleAdmin from "../tools/dummy_data";
 import axios from "axios";
 
 /**
@@ -9,11 +8,11 @@ import axios from "axios";
  * 
  * @returns {Node} Admin
  */
-const Admin = ({mediaId}) => {
+const Admin = () => {
     const {userId, setUserId} = useContext(UserContext);
 
     useEffect(() => {
-        axios.get(`/api/retrieve-info/${mediaId}`)
+        axios.get("/")
         .then(data => console.log(data))
         .catch(error => console.log(error));
     });
@@ -21,56 +20,7 @@ const Admin = ({mediaId}) => {
     return (<>
         {userId ?
             <section>
-                <div style={{border: "1px solid grey"}}>
-                    <div>
-                        <img src="" alt="Play button default" />
-                    </div>
-                    
-                    <div className="wavForm">
-                        <small>00:00</small>
-                        <small>48:12</small>
-                    </div>
-
-                    <div>
-                        <h3>Team Name</h3>
-                        <h2>Project Name</h2>
-                        <p>This will have some kind of text, I dunno</p>
-                    </div>
-                </div>
-
-                <div style={{border: "1px solid grey"}}>
-                    <nav>
-                        <button>Share</button>
-                        <button>Make A Note</button>
-                        <button>Delete</button>
-                    </nav>
-
-                    <nav>                    
-                        <button>Share</button>
-                        <button>Make A Note</button>
-                        <button>Delete</button>
-                    </nav>
-                </div>
-
-                <div style={{border: "1px solid grey"}}>
-                    <img src="" alt=""/>
-                    <p>Some text will go here, the description of the media itself. The other description will be about the project (which can have more than one media work).</p>
-                    <div style={{backgroundColor: "purple", width: "35px"}}>Tag</div>
-                </div>
-                <div style={{border: "1px solid grey"}}>
-                    Write A Note
-                </div>
-                <div style={{border: "1px solid grey"}}>
-                    <div>
-                        <img src="" alt="" />
-                    </div>
-                    <div>
-                        <h4>Duane Frank</h4>
-                        <p>
-                            Lorem ipsum orem ipsum orem ipsum orem ipsum orem ipsum orem ipsum. Lorem ipsum orem ipsum orem ipsum orem ipsum orem ipsum orem ipsum orem ipsum orem ipsum. Lorem ipsum orem ipsum orem ipsum.                        
-                        </p>
-                    </div>
-                </div>
+                {/** TBD */}
             </section> : <Home />
         }
     </>);
