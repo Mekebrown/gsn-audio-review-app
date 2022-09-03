e0675a818801bf345103bbcad59c2266508a5a51
Meke Brown <mekesiabrown@gmail.com>
2022-09-03 17:43:52 -0400

    build: add component to show info on a single user

---
+++ client/src/components/Admin/ShowSingleUser.js
@@ -0,0 +1,36 @@
+import React, { useContext, useEffect } from "react";
+import { UserContext } from "../../UserLogin";
+import axios from "axios";
+
+/**
+ * This page will show every note pertaining to every project that exists.
+ * 
+ * @returns {Node} ShowSingleUser
+ */
+const ShowSingleUser = () => {
+    const {userId} = useContext(UserContext);
+
+    useEffect(() => {
+        axios.get("/api/retrieve-info/all")
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
+    });
+    
+    return (
+        <section>
+            <header>
+                <h2>Single Note from user [USER NAME], UID #{userId}</h2>
+            </header>
+
+            Note body
+
+            Note timestamp
+
+            created on 
+
+            Updated? No/Yes, on datetime 
+        </section>
+    )
+}; 
+
+export default ShowSingleUser;
