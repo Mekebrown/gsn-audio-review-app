296f6b6d6bc2c36ae7888b3b8d287716999d957a
Meke Brown <mekesiabrown@gmail.com>
2022-09-03 17:05:27 -0400

    build: add func for timestamp and func for handling digest

--- client/src/components/tools/helper_functions.js
+++ client/src/components/tools/helper_functions.js
@@ -11,6 +11,60 @@ const sources = [
     {item: 2, ext: "ogg", type: "audio/ogg"}
 ];
 
+// Convert audioPoint to a string of a human-readable timestamp, as this value's example shows
+const getMarker = (isAudioPlayed, audioPlayer, noteStartDate, project) => { 
+    if (!isAudioPlayed) return false; 
+
+    let audioMinutes = Math.floor(audioPlayer.currentTime / 60); // Use audioPlayer.duration?
+    let audioSeconds = Math.floor(audioPlayer.currentTime % 60);
+
+    if (audioMinutes < 10) {
+        audioMinutes = "0" + audioMinutes;
+    } else if (audioMinutes > 59) {
+        audioMinutes = audioMinutes / 60;
+        audioMinutes = audioMinutes < 9 ? "0" + audioMinutes : audioMinutes;
+    }
+
+    if (audioSeconds < 10) {
+        audioSeconds = ":0" + audioSeconds;
+    } else if (audioSeconds > 59) {
+        audioSeconds = audioSeconds / 60;
+        
+        if (audioSeconds < 9) { 
+            audioSeconds = ":0" + audioSeconds;
+            audioMinutes = "01";
+        }
+    } else audioSeconds = ":" + audioSeconds;
+
+    let marker = audioMinutes + audioSeconds;
+            // Ex: 12122021_gsn_03:10:00
+    let noteID = noteStartDate + "_" + project + "_" + marker;
+
+    return marker, noteID;
+}
+const digest = () => {
+    if ("admin is webmaster") {
+        /**
+         * Email every app change
+         */
+    }
+
+    if ("admin is Lance and digest was not already sent out") {
+        /** 
+         * mailto: admin@something.net
+         * mailfrom: thesite
+         * subject: Digest
+         * body: 
+         *      Relevant info:
+         *      - User saved # of notes in a day
+         *      - Guest visits per day to the app
+         *      - User visits per day to the app
+         *      - Guest visits per day to a project
+         *      - User visits per day to a project
+         * */
+    }
+};
+
 const UserContext = createContext();
 
 export default values;
