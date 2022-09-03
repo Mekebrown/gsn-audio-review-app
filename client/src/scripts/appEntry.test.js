0d44186ab56dd55187e3482b3ef45d81830d8d25
Meke Brown <mekesiabrown@gmail.com>
2022-09-03 14:27:22 -0400

    build: delete files

--- client/src/scripts/appEntry.test.js
+++
@@ -1,5 +0,0 @@
-/** APP ENTRY */ 
-
-// Test 1 - Drop down choice of "Guest" does not enable keypass element
-// Test 2 - Drop down choice of "Reviewer" enables keypass element (#keypass)
-// Test 3 - Drop down choice of "Admin Log In" enables for the user the secret phrase input (#secretPhrase and #secretImage)
