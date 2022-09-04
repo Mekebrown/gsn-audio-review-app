8fc9e31109944b4fa331fda621890a7b929453e6
Meke Brown <mekesiabrown@gmail.com>
2022-09-04 01:53:50 -0400

    refactor: update variables and components for routing

--- client/src/App.js
+++ client/src/App.js
@@ -5,30 +5,39 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import { 
-  adminIndvlViewinglePath,
-  indexPath, 
-  userSingleMediaPath,
-  userPath, 
-  uploadMediaPath,
-  adminIndvlNote,
-  mediaId,
-  adminShowAllUsers,
-  adminPath 
-} from "./components/tools/vars";
-import UserSingleProject from "./components/User/UserSingleProject";
-import UploadMedia from "./components/Admin/UploadMedia";
-import Home from "./components/Home";
-import AllProjects from "./components/Admin/AllProjects";
 import logo from "./components/tools/logo.png";
+import {
+  indexPath,
+  adminPath,
+  adminIndvlViewinglePath,
+  adminIndvlNote,
+  uploadMediaPath,
+  adminSendPW,
+  adminShowAllUsers,
+  adminShowSingleUser,
+  adminShowAllNotes,
+  userPath,
+  userSingleMediaPath,
+  mediaId,
+} from "./components/tools/vars";
 
 import styles from "./App.css";
 import classNames from "classnames/bind";
 
+import Home from "./components/Home";
+
+import AdminShowAllProjects from "./components/Admin/AdminShowAllProjects";
 import AdminSingleProject from "./components/Admin/AdminSingleProject";
 import AdminSingleNote from "./components/Admin/AdminSingleNote";
-import ShowAllUsers from "./components/Admin/ShowAllUsers";
+import AdminShowAllUsers from "./components/Admin/AdminShowAllUsers";
+import UploadMedia from "./components/Admin/AdminUploadMedia";
+import AdminSendPW from "./components/Admin/AdminSendPW";
+import AdminShowSingleUser from "./components/Admin/AdminShowSingleUser";
+import AdminShowAllNotes from "./components/Admin/AdminShowAllNotes";
+
+import UserSingleProject from "./components/User/UserSingleProject";
 import UserAllMediaToReview from "./components/User/UserAllMediaToReview";
+
 import { UserLoginProvider } from "./UserLogin";
 
 const App = () => {
@@ -44,7 +53,11 @@ const App = () => {
   let searchBarInput = cx({ hideOption: userId});
 
   useEffect(() => {
+    let storedUserId = window.localStorage.getItem("userId");
 
+    if (storedUserId && !userId) {
+      setUserId(storedUserId);
+    }
   }, []);
 
   return (
@@ -61,27 +74,27 @@ const App = () => {
               </li>
             </section>
 
-            <section>
+            <section className={topBarLinksCont}>
                 <i className="fa fa-upload"></i>{' '}
-                <Link to={uploadMediaPath}>Upload</Link>
+                <Link to={uploadMediaPath} className={topBarLinks}>Upload</Link>
                 {' '} &nbsp; {' '} &nbsp; 
                 <i className="fa fa-search"></i>
-                <input className={searchBarInput} type="text" id="searchBar" title="searchBar" name="searchBar" placeholder="" />
+                <input className={`${searchBarInput} ${topBarLinksCont}`} type="text" id="searchBar" title="searchBar" name="searchBar" placeholder="" />
                 {' '} &nbsp; {' '} &nbsp; 
-                <Link to="/">                  
+                <Link to="/" className={topBarLinks}>                  
                   <i className="fa fa-user"></i>
                   Username
                 </Link>
                 {' '} &nbsp; {' '} &nbsp; 
-                <Link to="/">
+                <Link to="/" className={topBarLinks}>
                   <i className="fa fa-cog"></i>
                 </Link>
                 {' '} &nbsp; {' '} &nbsp; 
-                <Link to="/">
+                <Link to="/" className={topBarLinks}>
                   <i className="fa fa-comments"></i>
                 </Link>
                 {' '} &nbsp; {' '} &nbsp; 
-                <Link to="/">
+                <Link to="/" className={topBarLinks}>
                   <i className="fa fa-bell"></i>
                 </Link>
                 <button onClick={() => {setUserId(null); window.localStorage.removeItem('userId')}}>Log Out</button>
@@ -97,7 +110,10 @@ const App = () => {
         <Link to={adminIndvlViewinglePath}>Admin - Single Media Information</Link><br /> {/** /admin/retrieve-info/media/media_id */}
         <Link to={adminIndvlNote}>Admin - Show individual note</Link><br /> {/** /admin/review-info/note/:note_id */}
         <Link to={uploadMediaPath}>Admin - Upload Media</Link><br /> {/** /review/add-media */}
-        <Link to={adminShowAllUsers}>Admin - All Users</Link><br /><br />{/** /admin/users */}
+        <Link to={adminSendPW}>Admin - Send A Password</Link><br />{/**  */}
+        <Link to={adminShowAllUsers}>Admin - All Users</Link><br />{/** /admin/users */}
+        <Link to={adminShowSingleUser}>Admin - Show Single User</Link><br /> {/** /admin/retrieve-info/user */}
+        <Link to={adminShowAllNotes}>Admin - Show All Notes</Link><br /><br />{/** /admin/users */}
         
         <Link to={userPath}>User - All Media to Review</Link><br /> {/** /review */}
         <Link to={userSingleMediaPath}>User - Single Media Note Page</Link><br /> {/** /review/:media_id */}
@@ -110,11 +126,14 @@ const App = () => {
               {/* Notes to be used inside of AllProjects and AdminSingleProject components */}
               {/* AudioPlayerAndControls to be used inside of UserSingleProject component */}
               {/* NoteTaker to be used inside of UserSingleProject component */}
-              <Route path={adminPath} element={ <AllProjects />} />
-              <Route path={uploadMediaPath} element={ <UploadMedia />} />
-              <Route path={`${adminIndvlNote}/${mediaId}`} element={ <AdminSingleNote /> } />
+              <Route path={adminPath} element={ <AdminShowAllProjects />} />
               <Route path={`${adminIndvlViewinglePath}/${mediaId}`} element={ <AdminSingleProject /> } />
-              <Route path={adminShowAllUsers} element={ <ShowAllUsers /> } />
+              <Route path={`${adminIndvlNote}/${mediaId}`} element={ <AdminSingleNote /> } />
+              <Route path={uploadMediaPath} element={ <UploadMedia />} />
+              <Route path={adminSendPW} element={ <AdminSendPW /> } />
+              <Route path={adminShowAllUsers} element={ <AdminShowAllUsers /> } />
+              <Route path={adminShowSingleUser} element={ <AdminShowSingleUser /> } />
+              <Route path={adminShowAllNotes} element={ <AdminShowAllNotes /> } />
 
               <Route path={userPath} element={ <UserAllMediaToReview />} />
               <Route path={userSingleMediaPath} element={ <UserSingleProject mediaId={mediaId} />} />
