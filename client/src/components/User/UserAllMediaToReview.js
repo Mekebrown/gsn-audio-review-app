0150875ccd0b9346781ffb0ce2a2bb036ab13729
Meke Brown <mekesiabrown@gmail.com>
2022-09-04 01:51:08 -0400

    build: update with conditional based on logged in user id

--- client/src/components/User/UserAllMediaToReview.js
+++ client/src/components/User/UserAllMediaToReview.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useContext } from "react";
 import allMediaToReview from "../tools/dummy_data";
+import { UserContext } from "../../UserLogin";
+import Home from "../Home";
 
 /**
  * This page will show every user and their notes.
@@ -7,39 +9,43 @@ import allMediaToReview from "../tools/dummy_data";
  * @returns {Node} UserAllMediaToReview
  */
 const UserAllMediaToReview = () => {
-    return (
-        <section>
-            <header>
-                <h2>All Media to Review</h2>
-            </header>
+    const {userId} = useContext(UserContext);
 
-            {allMediaToReview.map(track => 
-                <section key={track[0].key}>
-                    <h3>
-                        <a href="/">{track[0].userName}</a>
-                    </h3>
+    return (<>
+        {userId ? 
+            <section>
+                <header>
+                    <h2>All Media to Review</h2>
+                </header>
 
-                    <ul>
-                        {track[0].usersNotes.map(note => 
-                        <li key={note.id} className="clickable tooltip">
-                            <h4>Project: {note.project}</h4>
+                {allMediaToReview.map(track => 
+                    <section key={track[0].key}>
+                        <h3>
+                            <a href="/">{track[0].userName}</a>
+                        </h3>
 
-                            <p>Posted note on: {note.posted}</p>
+                        <ul>
+                            {track[0].usersNotes.map(note => 
+                            <li key={note.id} className="clickable tooltip">
+                                <h4>Project: {note.project}</h4>
 
-                            <figure>
-                                <blockquote>
-                                    <p><a href="/">
-                                        {note.content}
-                                    </a></p>
-                                </blockquote>
-                            </figure>
-                            <hr />
-                        </li>)}
-                    </ul>
-                </section>
-            )}
-        </section>
-    )
+                                <p>Posted note on: {note.posted}</p>
+
+                                <figure>
+                                    <blockquote>
+                                        <p><a href="/">
+                                            {note.content}
+                                        </a></p>
+                                    </blockquote>
+                                </figure>
+                                <hr />
+                            </li>)}
+                        </ul>
+                    </section>
+                )}
+            </section> : <Home />
+        }
+    </>);
 }; 
 
 export default UserAllMediaToReview;
