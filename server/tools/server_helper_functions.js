1f1dc8a939b96179d50a60c8b807468bf730d7aa
Meke Brown <mekesiabrown@gmail.com>
2022-09-15 17:17:13 -0400

    refactor: revamp from doc reading/file obj values

--- server/tools/server_helper_functions.js
+++ server/tools/server_helper_functions.js
@@ -1,51 +1,24 @@
-const { S3Client, GetObjectCommand, ListObjectsCommand, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 require('dotenv').config();
-const path = require('path');
-const fs = require("fs");
 
-const run = async () => {
-    const get_params = {
+const s3Client = new S3Client({ region: process.env.S3_REGION });
+
+const setMediaStorage = async (file) => {
+    if (!s3Client) return;
+
+    const params = {
         Bucket: "gsnaudioreviewapp",
-        Key: "media/ten_algos.txt"
-    };
-
-    console.log("\n=======================================\n");
-
-    try {
-        const streamToString = stream => {
-            new Promise((resolve, reject) => {
-                const chunks = [];
-                stream.on("data", chunk => chunks.push(chunk));
-                stream.on("error", reject);
-                stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf8")));
-            });
-        };
-
-        const data = await s3Client.send(new GetObjectCommand(get_params));
-
-        const bodyContents = await streamToString(data.Body);
-
-        console.log(bodyContents);
-    } catch (err) { console.log("Error", err); }
-    console.log("\n=======================================\n");
-};
-
-const setMediaStorage = async (file_name, file) => {
-    const s3Client = new S3Client({ region: process.env.S3_REGION });
-
-    const fileStream = fs.createReadStream(file);
-
-    const put_params = {
-        Bucket: "gsnaudioreviewapp",
-        Key: file_name,
-        Body: fileStream,
+        ACL: "public-read",
+        Key: file.mimetype + "/" + file.name,
+        Body: file.data,
+        ContentType: file.mimetype,
     };
 
     try {
-        const results = await s3Client.send(new PutObjectCommand(put_params));
+        const results = await s3Client.send(new PutObjectCommand(params));
 
-        return results;
-    } catch (err) { console.log("Error", err); }
+        return results.$metadata.httpStatusCode;
+    } catch (err) { console.log(err); }
 };
 
 module.exports = { setMediaStorage };
