85e1b16f2cc1e6d4e4809664bbf1fb0631464904
Meke Brown <mekesiabrown@gmail.com>
2022-09-17 21:22:08 -0400

    refactor: add axios call, useEffect

--- client/src/components/Admin/AdminShowAllUsers.js
+++ client/src/components/Admin/AdminShowAllUsers.js
@@ -1,7 +1,8 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { allUsers } from "../tools/dummy_data";
 import { UserContext } from "../../UserLogin";
 import Home from "../Home";
+import axios from "axios";
 
 /**
  * This page will show every user and their notes.
@@ -9,7 +10,17 @@ import Home from "../Home";
  * @returns {Node} AdminShowAllUsers
  */
 const AdminShowAllUsers = () => {
-    const {userId} = useContext(UserContext);
+    const { userId } = useContext(UserContext);
+
+    useEffect(() => {
+        axios.get("/api/users")
+            .then(data => {
+                console.log(data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, []);
 
     return (<>
         {userId ?
@@ -18,34 +29,34 @@ const AdminShowAllUsers = () => {
                     <h2>Users</h2>
                 </header>
 
-                {allUsers.map(user => 
+                {allUsers.map(user =>
                     <section key={user.key}>
                         <h3>
                             <a href="/">{user.userName}</a>
                         </h3>
 
                         <ul>
-                            {user.usersNotes.map(note => 
-                            <li key={note.id} className="clickable tooltip">
-                                <h4>Project: {note.project}</h4>
+                            {user.usersNotes.map(note =>
+                                <li key={note.id} className="clickable tooltip">
+                                    <h4>Project: {note.project}</h4>
 
-                                <p>Posted note on: {note.posted}</p>
+                                    <p>Posted note on: {note.posted}</p>
 
-                                <figure>
-                                    <blockquote>
-                                        <p><a href="/">
-                                            {note.content}
-                                        </a></p>
-                                    </blockquote>
-                                </figure>
-                                <hr />
-                            </li>)}
+                                    <figure>
+                                        <blockquote>
+                                            <p><a href="/">
+                                                {note.content}
+                                            </a></p>
+                                        </blockquote>
+                                    </figure>
+                                    <hr />
+                                </li>)}
                         </ul>
                     </section>
                 )}
             </section> : <Home />
         }
     </>);
-}; 
+};
 
 export default AdminShowAllUsers;
