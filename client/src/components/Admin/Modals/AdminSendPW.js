a8a706ba195ffca177b0dfa6d81dbf158839da7c
Meke Brown <mekesiabrown@gmail.com>
2022-09-17 21:19:10 -0400

    refactor: update axios url; add comment

--- client/src/components/Admin/Modals/AdminSendPW.js
+++ client/src/components/Admin/Modals/AdminSendPW.js
@@ -5,6 +5,7 @@ import Home from "../../Home";
 
 /**
  * This page will show every note pertaining to every project that exists.
+ * Have option to email user the password.
  * 
  * @returns {Node} AdminSendPW
  */
@@ -12,7 +13,7 @@ const AdminSendPW = () => {
     const { userId } = useContext(UserContext);
 
     useEffect(() => {
-        axios.get("/api/retrieve-info/all")
+        axios.get("/api/send-pw")
             .then(data => console.log(data))
             .catch(error => console.log(error));
     });
