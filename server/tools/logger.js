e010cc5bc25384dfbf8ad7053433b7080f729dd7
Meke Brown <mekesiabrown@gmail.com>
2022-09-20 11:43:24 -0400

    refactor: move log file

---
+++ server/tools/logger.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+module.exports = function logger(details) {
+  let current = ((new Date()).toLocaleString()).replace(/\D*/g, "");
+  let file_name = `./files/logs/${details.desc}${current}.log`;
+
+  let log_data = {
+    message: details.message,
+    req: details.req ? details.req : "N/A",
+    res: details.res ? details.res : "N/A",
+    headers: details.headers ? details.headers : "N/A",
+  };
+
+  fs.writeFile(file_name, JSON.stringify(log_data, null, "\t"), "utf8", (error, data) => {
+    console.log(`Write complete! Error: ${error} | Data: ${data}`);
+  });
+};
