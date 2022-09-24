1118c906a4a4d1d4aa24519a28e97cfbe5b93179
Meke Brown <mekesiabrown@gmail.com>
2022-09-24 19:02:31 -0400

    refactor: add navigation rerouting, http req, context

--- client/src/components/General/Logout.js
+++ client/src/components/General/Logout.js
@@ -1,4 +1,35 @@
+import { useEffect, useContext, useState } from "react";
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
+
+import UserGlobalContext from '../tools/UserGlobalContext';
+
 const Logout = () => {
+    const userGlobalContext = useContext(UserGlobalContext);
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        axios.post("/api/logout", null, {
+            withCredentials: true,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+            },
+        })
+            .then((response) => {
+                if (response.status === 200) {
+                    userGlobalContext.setUser({});
+
+                    navigate('/');
+                } else {
+                    throw new Error();
+                }
+            })
+            .catch((error) => {
+                console.error(`Couldn't log the user out: ${error}`);
+            });
+    }, []);
+
     return <div>Bye!</div>;
 };
 
