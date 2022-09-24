15057c32427fbf655b1a9c8f9dd7737d83167144
Meke Brown <mekesiabrown@gmail.com>
2022-09-24 17:58:50 -0400

    refactor: add helpful cors option values

--- server/tools/cors_options.js
+++ server/tools/cors_options.js
@@ -1,24 +1,26 @@
 const allowList = [
-    'http://localhost:3000/', 
-    'http://localhost:3001/', 
-    'https://intense-forest-28148.herokuapp.com/'
+  'http://localhost:3000/',
+  'http://localhost:3001/',
+  'https://intense-forest-28148.herokuapp.com/'
 ];
 
 
 const corsOptions = {
-    origin: (origin, callback) => {
-      console.log("** Origin of request " + origin);
-  
-      if (allowList.indexOf(origin) !== -1 || !origin) {
-        console.log("Origin acceptable");
-        callback(null, true);
-      } else {
-        console.log(allowList.indexOf(origin));
-        callback(new Error("Not allowed by CORS"));
-      }
+  origin: (origin, callback) => {
+    console.log("** Origin of request " + origin);
+
+    if (allowList.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable");
+      callback(null, true);
+    } else {
+      console.error(allowList.indexOf(origin));
+      callback(new Error("Not allowed by CORS"));
     }
+  },
+  methods: ['POST', 'GET', 'HEAD'],
+  credentials: true,
 };
 
 module.exports = {
-    corsOptions
-}
+  corsOptions
+};
