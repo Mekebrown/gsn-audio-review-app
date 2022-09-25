3fc56ee620a6e810fcf18c14ed053b08d47c453e
Meke Brown <mekesiabrown@gmail.com>
2022-09-25 01:29:11 -0400

    refactor: update handling of log out

--- client/src/components/General/Logout.js
+++ client/src/components/General/Logout.js
@@ -1,36 +1,24 @@
-import { useEffect, useContext, useState } from "react";
 import axios from "axios";
 import { useNavigate } from 'react-router-dom';
 
-import UserGlobalContext from '../tools/UserGlobalContext';
+import { useAuth } from '../tools/user-context/UserGlobalContextProvider';
 
 const Logout = () => {
-    const userGlobalContext = useContext(UserGlobalContext);
+    const { setUser } = useAuth();
 
     const navigate = useNavigate();
 
-    useEffect(() => {
-        axios.post("/api/logout", null, {
-            withCredentials: true,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-            },
+    axios.get("/api/logout")
+        .then((res) => {
+            if (res.status === 200) {
+                setUser({});
+
+                navigate('/login');
+            } else {
+                throw new Error();
+            }
         })
-            .then((response) => {
-                if (response.status === 200) {
-                    userGlobalContext.setUser({});
-
-                    navigate('/');
-                } else {
-                    throw new Error();
-                }
-            })
-            .catch((error) => {
-                console.error(`Couldn't log the user out: ${error}`);
-            });
-    }, []);
-
-    return <div>Bye!</div>;
+        .catch((error) => { console.error(`Couldn't log out: ${error}`); });
 };
 
 export default Logout;
