ed54cf4288bf6cc70b5c4d0b7451a7c2f92c4606
Mekesia Brown <mekesiabrown@gmail.com>
2023-04-18 01:44:32 -0400

    refactor: control for log in states

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,17 +1,90 @@
-export default function NavBar() {
-    return <nav
-        className="flex items-center justify-between flex-wrap bg-white p-6"
-        >
-            <div className="flex items-center flex-shrink-0 text-black mr-6">
+import React, { useState } from "react";
+import Link from "next/link";
+import Hamburger from "./menu/Hamburger";
+
+const VisitorNavBar = ({href = "/login"}) => {
+    return <menu className="flex items-center justify-between flex-wrap bg-white p-6">
+        <Link 
+            href={href} 
+            className="flex items-center flex-shrink-0 text-black mr-6">
+            <span className="font-semibold text-xl tracking-tight">
                 <img 
                     src="/logo192.png" 
                     alt="GSN Logo" 
                     className="h-8 mr-2" 
                 />
+                GSN Audio Review App
+            </span>
+        </Link>
+    </menu>;
+};
 
+const AdminNavBar = ({ href = "/", open, setOpen }) => {
+    return <menu className="flex items-center justify-between flex-wrap bg-white p-6">
+        <section className=" text-black">
+            <Link 
+                href={href}
+            >
                 <span className="font-semibold text-xl tracking-tight">
-                    GSN Audio Review App
+                    <img 
+                        src="/logo192.png" 
+                        alt="GSN Logo" 
+                        className="h-8 mr-2 inline" 
+                    />
+                    Admin View
                 </span>
-            </div>
-        </nav>;
+            </Link>
+        </section>
+
+        <section className="text-gray">
+            <Hamburger role="admin" />
+        </section>
+    </menu>;
+};
+
+const UserNavBar = ({href = "/", open, setOpen}) => {
+    return <menu className="flex items-center justify-between flex-wrap bg-white p-6">
+        <section className="text-black">
+            <Link
+                href={href}
+            >
+                <span className="font-semibold text-xl tracking-tight">
+                    <img 
+                        src="/logo192.png" 
+                        alt="GSN Logo" 
+                        className="h-8 mr-2 inline" 
+                    />
+                    User View
+                </span>
+            </Link>
+        </section>
+
+        <section className="text-gray">
+            <Hamburger role="user" />
+        </section>
+    </menu>;
+};
+
+const NavBar = () => {
+    const [loggedIn, setLoggedIn] = useState("admin");
+
+    const ShowNavBar = () => {
+        if (loggedIn === "admin") {
+            return <AdminNavBar />;
+        } else if (loggedIn === "user") {
+            return <UserNavBar />;
+        } else {
+            return <VisitorNavBar />;
+        }
+    };
+
+    return <header className="box-sizing: border-box; margin: 0 auto 3em;">
+        <nav
+        className="bg-white p-6"
+        >
+            <ShowNavBar />
+        </nav>
+    </header>;
 }
+
+export default NavBar;
