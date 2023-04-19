d6ec3041c32241656bda2ab364210f5da618e719
Mekesia Brown <mekesiabrown@gmail.com>
2023-04-19 14:48:37 -0400

    refactor: update component paths

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,7 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
-import NavBar from '../components/NavBar';
-import Footer from '../components/Footer';
+import NavBar from '../components/menu/NavBar';
+import Footer from '../components/menu/Footer';
 
 export default function Document() {
   return <Html lang="en">
