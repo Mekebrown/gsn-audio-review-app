0e1946f06ea533c2febefea5042c1f942a211505
Mekesia Brown <mekesiabrown@gmail.com>
2023-05-02 19:04:00 -0400

    build: add main routes/pages

--- components/modals/ShowNoteModal.jsx
+++ components/modals/ShowNoteModal.jsx
@@ -10,4 +10,15 @@ const ShowNoteModal = ({ note, onClose }) => {
   );
 }
 
+const CreateNewNoteModal = ({ onClose }) => {
+  return (
+    <Modal onClose={onClose}>
+      <h1>Create New Note</h1>
+      <p>Form goes here</p>
+    </Modal>
+  );
+};
+
 export default ShowNoteModal;
+
+export { CreateNewNoteModal };
