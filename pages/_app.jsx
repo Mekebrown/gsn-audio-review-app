eb088e6db33de43c2716727d02c0ca3fa7b98b53
Mekesia Brown <mekesiabrown@gmail.com>
2023-05-02 18:48:15 -0400

    build: add auth provider for context

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import Head from 'next/head';
 
+import AuthContextProvider from "../lib/context/AuthContext";
 import '@/styles/globals.css';
 
 function App({ Component, pageProps }) {
-  return <>
+  return <AuthContextProvider>
       <Head>
         <title>GSN Audio Review App</title>
       </Head>
       
       <Component {...pageProps} />
-  </>;
+  </AuthContextProvider>;
 }
 
 export default App;
