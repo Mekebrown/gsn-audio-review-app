f0629c46401c8220cb7e69be16ad0a890740298a
Mekesia Brown <mekesiabrown@gmail.com>
2023-05-07 01:50:27 -0400

    refactor: expand on login code

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,7 +1,38 @@
+const bcrypt = require("bcrypt");
+
+const client = require("../../lib/db/pg_pr_connect");
 const logger = require("../../lib/logger");
+const { login_query } = require("../../lib/db/pg_pr_query_strings");
 
 export default function handler(req, res) {
     if (req) {
+        client.query(login_query, [username], (err, res) => {
+            if (err) {
+                logger({
+                    desc: "login query",
+                    req: "Body: " + JSON.stringify(req.body),
+                    res: "N/A",
+                    headers: "N/A",
+                    message: JSON.stringify(err)
+                });
+
+                return done(err);
+            }
+
+            if (res.rows.length === 0) {
+                return done(null, false, { message: 'Incorrect login information.' });
+            }
+
+            const user = res.rows[0];
+            const hashed_password = user.hashed_password;
+
+            bcrypt.compare(password, hashed_password).then(function (result) {
+                if (!result) return done(null, false, { message: 'Incorrect password.' });
+
+                return done(null, user);
+            });
+        });
+
         res.status(200).json({ name: 'John Doe' });
     } else {
         logger({
