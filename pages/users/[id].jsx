ba02d77d0522de4bbe56e04ec471fc9c1a4984a8
Mekesia Brown <mekesiabrown@gmail.com>
2023-07-16 15:39:58 -0400

    chore: note a TODO to add account(s) of each user

--- pages/users/[id].jsx
+++ pages/users/[id].jsx
@@ -4,6 +4,7 @@ export default function User({ content }) {
   return <div>User #{content.id}</div>;
 }
 
+// TODO: Need to get every Account for this User
 export async function getStaticProps() {
   // const { content } = await fetch('/api/users/:id').then((res) => res.json());
   const content = { id: 6 };
