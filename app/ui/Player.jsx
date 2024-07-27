d12e28e8e9d8de7845de06691bc994db1b1e0529
Mekesia Brown <mekesiabrown@gmail.com>
2024-07-27 13:39:59 -0400

    refactor: update player's html elements

--- app/ui/Player.jsx
+++ app/ui/Player.jsx
@@ -1,14 +1,24 @@
 /**
- * @description Component for a media player 
+ * @description Component for a media player
  *
  * @component
  * 
  * @returns {JSX.Element} <Player />
  */
-export default function Player({ title, track }) {
-    return <figure>
-        <figcaption>{title}</figcaption>
+export default function Player({ title, track, desc, captions }) {
+    return <>
+        <details>
+            <summary>{title}</summary>
 
-        <audio controls src={track}></audio>
-    </figure>;
+            <p>{desc}</p>
+            <time dateTime="2008-02-14 20:00">2/14/2024</time>
+        </details>
+
+        <audio controls>
+            <source src={track[0]} type="audio/mpeg"></source>
+            <source src={track[1]} type="audio/ogg"></source>
+            <track src={captions} kind="subtitles" srcLang="en" label="English"></track>
+            Your browser does not support the audio element.
+        </audio>
+    </>;
 };
