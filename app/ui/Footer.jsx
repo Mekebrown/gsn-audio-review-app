46094002a36ec66c67562ae00150e988e53460cf
Mekesia Brown <mekesiabrown@gmail.com>
2024-07-27 13:43:48 -0400

    refactor: update footer  code

--- app/ui/Footer.jsx
+++ app/ui/Footer.jsx
@@ -1,88 +1,95 @@
 import Link from 'next/link';
 
 import { GSNLogo } from '@/app/lib/general_variables';
+import "@/styles/ui/footer.css";
 
 export default function Footer() {
     return <footer>
         {/* brand bio/mission - subs box - IG feed? - social media - location -  secondary nav links */}
-        <section>
-            <Link
-                href="/"
-                rel="noopener noreferrer"
-            >
-                <span> Powered by <GSNLogo /> </span>
-            </Link>
-            <br />
-            <p>Thanks for listening!</p><br />
+        <section className="footerTopSection">
+            <div className="footerTopLeftCol">
+                <Link href="/" rel="noopener noreferrer" >
+                    <span><GSNLogo /></span>
+                </Link>
 
-            <small>
-                Save notes, follow media in transition, and contribute to content quality. All in real time! <br />
-            </small>
-            <br />
-            <Link
-                href="/signin"
-                rel="noopener noreferrer"
-            >
-                Sign In
-            </Link><br />
-            <br />
-            <hr className="horizontalRule" />
+                <p>Thanks for listening!</p>
 
-            <Link
-                href="https://www.mekesiabrown.com"
-                rel="noopener noreferrer"
-            >
-                Developed by Meke
-            </Link>
+                <small>
+                    Save notes, follow media in transition, and contribute to content quality. All in real time! <br />
+                </small>
+            </div>
 
-            <Link
-                href="/about/gsn"
-                rel="noopener noreferrer"
-            >
-                About GSN
-            </Link> -
+            <div className="footerTopRightCol">
+                <Link href="/contact" rel="noopener noreferrer" className="footerContactUsBtn">
+                    CONTACT US
+                </Link>
+                &nbsp;
+                <Link href="/signin" rel="noopener noreferrer" className="footerSignInBtn">
+                    SIGN IN
+                </Link>
+            </div>
+        </section>
 
-            <Link
-                href="/about/gmp"
-                rel="noopener noreferrer"
-            >
-                About GMP
-            </Link> -
+        <section className="footerBottomSection">
+            <div className="footerBottomLeftCol">
+                <Link
+                    href="https://www.mekesiabrown.com"
+                    rel="noopener noreferrer"
+                >
+                    <small>Developed by Gifted</small>
+                </Link>
+            </div>
 
-            <Link
-                href="/blog"
-                rel="noopener noreferrer"
-            >
-                Blog
-            </Link> -
-
-            <Link
-                href="/contact"
-                rel="noopener noreferrer"
-            >
-                Contact Us
-            </Link> -
-
-            <Link
-                href="/faq"
-                rel="noopener noreferrer"
-            >
-                FAQ
-            </Link> -
-
-            <Link
-                href="/terms"
-                rel="noopener noreferrer"
-            >
-                Terms
-            </Link> -
-
-            <Link
-                href="/disclaimer"
-                rel="noopener noreferrer"
-            >
-                Disclaimer
-            </Link>
+            <div className="footerBottomRightCol">
+                <Link
+                    href="/about/gsn"
+                    rel="noopener noreferrer"
+                >
+                    About GSN
+                </Link>
+                &nbsp; - &nbsp;
+                <Link
+                    href="/about/gmp"
+                    rel="noopener noreferrer"
+                >
+                    About GMP
+                </Link>
+                &nbsp; - &nbsp;
+                <Link
+                    href="/blog"
+                    rel="noopener noreferrer"
+                >
+                    Blog
+                </Link>
+                &nbsp; - &nbsp;
+                <Link
+                    href="/contact"
+                    rel="noopener noreferrer"
+                >
+                    Contact Us
+                </Link>
+                &nbsp; - &nbsp;
+                <Link
+                    href="/about/ntks/faq"
+                    rel="noopener noreferrer"
+                >
+                    FAQ
+                </Link>
+                &nbsp; - &nbsp;
+                <Link
+                    href="/about/ntks/terms"
+                    rel="noopener noreferrer"
+                >
+                    Terms
+                </Link>
+                &nbsp; - &nbsp;
+                <Link
+                    href="/about/ntks/disclaimer"
+                    rel="noopener noreferrer"
+                >
+                    Disclaimer
+                </Link>
+            </div>
         </section>
     </footer>;
 };
